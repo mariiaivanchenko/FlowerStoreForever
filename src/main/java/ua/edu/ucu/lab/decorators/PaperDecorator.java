98e021d823ed8480aeadc485cf1ddd5909c23b5f
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-11-10 19:42:40 +0200

    Update PaperDecorator.java

--- src/main/java/ua/edu/ucu/lab/decorators/PaperDecorator.java
+++ src/main/java/ua/edu/ucu/lab/decorators/PaperDecorator.java
@@ -2,7 +2,7 @@ package ua.edu.ucu.lab.decorators;
 
 import ua.edu.ucu.lab.flower.Item;
 
-public class PaperDecorator extends ItemDecorator{
+public class PaperDecorator extends ItemDecorator {
     public PaperDecorator(Item item) {
         super(item);
     }
