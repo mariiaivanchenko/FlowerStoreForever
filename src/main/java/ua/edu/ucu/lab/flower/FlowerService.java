565c3946af581f82762552f40ecc7f094d2aa9fe
mariiaivanchenko <116714261+mariiaivanchenko@users.noreply.github.com>
2023-11-10 19:44:27 +0200

    Update FlowerService.java

--- src/main/java/ua/edu/ucu/lab/flower/FlowerService.java
+++ src/main/java/ua/edu/ucu/lab/flower/FlowerService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class FlowerService {
 
-    public FlowerRepository flowerRepository;
+    private FlowerRepository flowerRepository;
 
     @Autowired
     public FlowerService(FlowerRepository flowerrepository) {
